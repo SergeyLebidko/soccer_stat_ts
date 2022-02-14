3bbb7126ab21c3805ce437605edaf7f78427748f
SergeyLebidko <it.sergeyler@mail.ru>
2022-02-14 15:04:51 +0300

    Создана заготовка для Main

---
+++ src/components/pages/Main/Main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Main.scss';
+
+const Main: React.FC = () => {
+  return <div>Главная страница сайта с его логотипом и кратким описанием</div>;
+};
+
+export default Main;
+
