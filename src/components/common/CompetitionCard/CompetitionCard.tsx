665c8518d29c9b05dbe96528e52b1f228a67438a
SergeyLebidko <it.sergeyler@mail.ru>
2022-02-14 16:50:05 +0300

    Добавлены карточки лиги, команды, матча

---
+++ src/components/common/CompetitionCard/CompetitionCard.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './CompetitionCard.scss';
+
+const CompetitionCard: React.FC = () => {
+  return <div>Карточка лиги</div>;
+};
+
+export default CompetitionCard;
---
+++ src/components/common/TeamCard/TeamCard.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './TeamCard.scss';
+
+const TeamCard: React.FC = () => {
+  return <div>Карточка команды</div>;
+};
+
+export default TeamCard;
