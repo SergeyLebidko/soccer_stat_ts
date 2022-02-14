a874482425030763eb583c19dc690882c8e8db93
SergeyLebidko <it.sergeyler@mail.ru>
2022-02-14 11:53:10 +0300

    Изменено расширение файлов стилей

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
-import './style/index.css';
+import './style/reset.scss';
 
 const root = document.getElementById('root');
 ReactDOM.render(<App/>, root);
