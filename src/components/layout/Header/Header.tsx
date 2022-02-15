ffd42b4ca045b1a3ed863f237c4a473dfdfc5bd4
SergeyLebidko <it.sergeyler@mail.ru>
2022-02-15 14:08:46 +0300

    Добавлены ссылки в Header

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import './Header.scss';
+import {Link, NavLink} from 'react-router-dom';
 
 const Header: React.FC = () => {
-  return <div>Заголовок страницы</div>;
+  return (
+    <div>
+      <Link to="/">Главная</Link>
+      <NavLink to="competition_list" style={({isActive}) => isActive ? {color: 'red'} : {}}>
+        Лиги
+      </NavLink>
+      <NavLink to="team_list" style={({isActive}) => isActive ? {color: 'red'} : {}}>
+        Команды
+      </NavLink>
+    </div>
+  );
 };
 
 export default Header;
