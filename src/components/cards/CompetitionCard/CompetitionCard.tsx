a76cf1884dd1c8e6f4236fccef575068e46fe87a
SergeyLebidko <it.sergeyler@mail.ru>
2022-02-16 13:50:12 +0300

    Добавлен обработчик ошибок загрузки изображений лиг

--- src/components/cards/CompetitionCard/CompetitionCard.tsx
+++ src/components/cards/CompetitionCard/CompetitionCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {TCompetition} from '../../../types';
 import './CompetitionCard.scss';
 
@@ -7,12 +7,16 @@ type CompetitionCardProps = {
 }
 
 const CompetitionCard: React.FC<CompetitionCardProps> = ({competition}) => {
+  const [hasEmblemError, setHasEmblemError] = useState<boolean>(false);
+
+  const emblemErrorHandler = () => setHasEmblemError(true);
+
   return (
     <li className="competition_card">
       <h1 className="competition_card__title">{competition.name}</h1>
-      {competition.emblemUrl === null ?
-        <div className="competition_card__no_emblem">нет фото...</div>:
-        <img className="competition_card__emblem" src={competition.emblemUrl}/>
+      {competition.emblemUrl === null || hasEmblemError ?
+        <div className="competition_card__no_emblem">нет фото...</div> :
+        <img className="competition_card__emblem" src={competition.emblemUrl} onError={emblemErrorHandler}/>
       }
       <h3 className="competition_card__country">{competition.area.name}</h3>
     </li>
