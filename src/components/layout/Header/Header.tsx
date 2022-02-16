f792f024c20cdec14e89d7031a1ebb710f36c014
SergeyLebidko <it.sergeyler@mail.ru>
2022-02-16 13:53:05 +0300

    Рефакторинг выбора класса в Header.tsx

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -5,19 +5,15 @@ import './Header.scss';
 import classNames from 'classnames';
 
 const Header: React.FC = () => {
+  const classSelector = (arg: { isActive: boolean }): string => {
+    return classNames('header__link', {'header__link_active': arg.isActive});
+  };
+
   return (
     <div className="header">
       <img className="header__logo" src={ball} alt="Главная страница"/>
-      <NavLink
-        to="competitions"
-        className={({isActive}) => classNames('header__link', {'header__link_active': isActive})}>
-        Лиги
-      </NavLink>
-      <NavLink
-        to="teams"
-        className={({isActive}) => classNames('header__link', {'header__link_active': isActive})}>
-        Команды
-      </NavLink>
+      <NavLink to="competitions" className={classSelector}>Лиги</NavLink>
+      <NavLink to="teams" className={classSelector}>Команды</NavLink>
     </div>
   );
 };
