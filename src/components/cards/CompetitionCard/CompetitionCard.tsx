f88f5644c07df11b8a5a92eeccaea9cdb8503874
SergeyLebidko <it.sergeyler@mail.ru>
2022-02-16 14:00:34 +0300

    Добавлен компонент NoPhoto.tsx

--- src/components/cards/CompetitionCard/CompetitionCard.tsx
+++ src/components/cards/CompetitionCard/CompetitionCard.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {TCompetition} from '../../../types';
+import NoPhoto from '../../common/NoPhoto/NoPhoto';
 import './CompetitionCard.scss';
 
 type CompetitionCardProps = {
@@ -15,7 +16,7 @@ const CompetitionCard: React.FC<CompetitionCardProps> = ({competition}) => {
     <li className="competition_card">
       <h1 className="competition_card__title">{competition.name}</h1>
       {competition.emblemUrl === null || hasEmblemError ?
-        <div className="competition_card__no_emblem">нет фото...</div> :
+        <NoPhoto/> :
         <img className="competition_card__emblem" src={competition.emblemUrl} onError={emblemErrorHandler}/>
       }
       <h3 className="competition_card__country">{competition.area.name}</h3>
---
+++ src/components/common/NoPhoto/NoPhoto.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './NoPhoto.scss';
+
+const NoPhoto: React.FC = () => {
+  return <div className="no_photo">нет фото...</div>;
+};
+
+export default NoPhoto;
