b982783ce6fd131841b393791542ad2b3df4ee02
SergeyLebidko <it.sergeyler@mail.ru>
2022-02-17 16:06:44 +0300

    К списку матчей добавлена пагинация

--- src/components/pages/Competition/Competition.tsx
+++ src/components/pages/Competition/Competition.tsx
@@ -6,12 +6,16 @@ import MatchCard from '../../cards/MatchCard/MatchCard';
 import {TMatch} from '../../../types';
 import {loadCompetitionCalendar} from '../../../utils';
 import './Competition.scss';
+import {PAGE_SIZE} from '../../../settings';
+import Paginator from '../../common/Paginator/Paginator';
 
 const Competition: React.FC = () => {
   const {id} = useParams();
 
   const [competitionName, setCompetitionName] = useState<string>('');
   const [matches, setMatches] = useState<Array<TMatch>>([]);
+
+  const [pageStart, setPageStart] = useState<number>(0);
 
   const [preloader, setPreloader] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
@@ -31,12 +35,20 @@ const Competition: React.FC = () => {
 
   if (error) return <Error error={error}/>;
 
+  let _matches = matches;
+  const searchedLength = _matches.length;
+
+  // Применяем пагинацию
+  _matches = _matches.filter((_, index) => index >= pageStart && index <= (pageStart + PAGE_SIZE - 1));
+
   return (
     <div className="competitions">
       {competitionName}
       <ul className="competitions__cards_block">
-        {matches.map(((match) => <MatchCard key={match.id} match={match}/>))}
+        {_matches.map(((match) => <MatchCard key={match.id} match={match}/>))}
       </ul>
+      {searchedLength > PAGE_SIZE &&
+      <Paginator pageStart={pageStart} total={searchedLength} setPage={setPageStart}/>}
     </div>
   );
 };
