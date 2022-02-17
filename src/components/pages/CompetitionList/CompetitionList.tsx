c5663b2ff6419f2b4c3624e1b09ed362c960e1ef
SergeyLebidko <it.sergeyler@mail.ru>
2022-02-17 10:46:39 +0300

    Рефакторинг CompetitionList.tsx

--- src/components/pages/CompetitionList/CompetitionList.tsx
+++ src/components/pages/CompetitionList/CompetitionList.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react';
+import React, {useState} from 'react';
 import {TCompetition} from '../../../types';
 import CompetitionCard from '../../cards/CompetitionCard/CompetitionCard';
 import SearchField from '../../common/SearchField/SearchField';
@@ -14,34 +14,33 @@ const CompetitionList: React.FC<CompetitionListProp> = ({list}) => {
   const [pageStart, setPageStart] = useState<number>(0);
   const [search, setSearch] = useState<string>('');
 
-  const createListForShow = useCallback(() => {
-    if (search.length === 0) return list;
-    const _search = search.toLowerCase();
-    return list.filter(({name: cName, area: {name: aName}}) => {
-      return cName.toLowerCase().includes(_search) || aName.toLowerCase().includes(_search);
-    });
-  }, [search, list]);
-
   const changeSearchHandler = (event: React.ChangeEvent): void => {
     setSearch((event.target as HTMLInputElement).value);
     setPageStart(0);
   };
 
-  const listForShow = createListForShow();
+  let _list = list;
+
+  // Отсекаем элементы, не подходящие под критерии поиска
+  if (search.length > 0) {
+    const _search = search.toLowerCase();
+    _list = list.filter(({name: cName, area: {name: aName}}) => {
+      return cName.toLowerCase().includes(_search) || aName.toLowerCase().includes(_search);
+    });
+  }
+  const searchedLength = _list.length;
+
+  // Применяем пагинацию
+  _list = _list.filter((_, index) => index >= pageStart && index <= (pageStart + PAGE_SIZE - 1));
 
   return (
     <div className="competition_list">
       <SearchField search={search} changeSearchHandler={changeSearchHandler}/>
       <ul className="competition_list__cards_block">
-        {listForShow
-            .filter((_, index) => index >= pageStart && index <= (pageStart + PAGE_SIZE - 1))
-            .map((competition) =>
-              <CompetitionCard key={competition.id} competition={competition}/>,
-            )
-        }
+        {_list.map((item) => <CompetitionCard key={item.id} competition={item}/>)}
       </ul>
-      {listForShow.length > PAGE_SIZE &&
-      <Paginator pageStart={pageStart} total={listForShow.length} setPage={setPageStart}/>}
+      {searchedLength > PAGE_SIZE &&
+      <Paginator pageStart={pageStart} total={searchedLength} setPage={setPageStart}/>}
     </div>
   );
 };
